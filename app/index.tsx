5dbad16d49ff448ff9173e53705e2943171a4715
Vitor Andreola <dev.andreola@gmail.com>
2025-05-08 18:10:51 -0300

    chore: install nativewind

--- app/index.tsx
+++ app/index.tsx
@@ -2,14 +2,8 @@ import { Text, View } from "react-native";
 
 export default function Index() {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Hello world!</Text>
+    <View className="flex-1 items-center justify-center bg-red-300">
+      <Text className="text-2xl font-bold">Hello world!</Text>
     </View>
   );
 }
