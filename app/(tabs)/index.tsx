6ad38d1779162193b1a44f4610a74abc585dc91f
Vitor Andreola <dev.andreola@gmail.com>
2025-05-08 20:15:52 -0300

    feat: create navigation tabs

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -2,7 +2,7 @@ import { Text, View } from "react-native";
 
 export default function Index() {
   return (
-    <View className="flex-1 items-center justify-center bg-red-300">
+    <View className="flex-1 items-center justify-center">
       <Text className="text-5xl text-dark-200 font-bold">Hello world!</Text>
     </View>
   );
