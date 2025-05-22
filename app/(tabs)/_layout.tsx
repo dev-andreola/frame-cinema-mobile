92511efd8fc339f90c7a7b794999eb4393c283fa
Vitor Andreola <dev.andreola@gmail.com>
2025-05-22 15:03:09 -0300

    fix: tab layout

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -9,7 +9,7 @@ function TabIcon({ focused, icon, title }: any) {
     return (
       <ImageBackground
         source={images.highlight}
-        className="flex flex-row w-full flex-1 min-w-[112px] min-h-[54px] mt-4 justify-center items-center rounded-full overflow-hidden"
+        className="flex flex-row w-full flex-1 min-w-[112px] min-h-[52px] mt-6 justify-center items-center rounded-full overflow-hidden"
       >
         <Image source={icon} tintColor="#151312" className="size-5" />
         <Text className="text-secondary text-base font-semibold ml-2">
@@ -41,7 +41,7 @@ export default function TabsLayout() {
           backgroundColor: "#0F0D23",
           borderRadius: 50,
           marginHorizontal: 30,
-          marginBottom: 36,
+          marginBottom: 70,
           height: 52,
           position: "absolute",
           borderWidth: 1,
